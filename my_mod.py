29eefa5704c4ab1d146c27a7019b004e150cf695
ppate304 <patel.pari.97@gmail.com>
2021-10-07 15:00:45 -0500

    first commit

---
+++ my_mod.py
@@ -0,0 +1,21 @@
+def area_house():
+    
+    length_house=float(input("What is the length? "))
+        
+    width_house=float(input("what is the width? "))
+      
+    return (length_house * width_house)
+        
+    
+def circumference_circle():
+    r = float(input("what is the radius? "))
+    circumference = 3.14 * (r**2)
+    return circumference
+              
+
+    
+    
+    
+        
+    
+   
